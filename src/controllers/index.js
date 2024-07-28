9ebaeb26f0df657434209b5cc3b5e3a9e03ffeb7
Shivam Anand <shivam850anand@gmail.com>
2024-07-28 11:01:34 +0530

    chat box auth codebase set up

---
+++ src/controllers/index.js
@@ -0,0 +1,17 @@
+import useremailverifyController from "./useremailverifyController.js";
+import userregisterController from "./userregisterController.js";
+import userloginController from "./userloginController.js";
+import userprofileController from "./userprofileController.js";
+import userlogoutController from "./userlogoutController.js";
+import userpasswordChangeController from "./userpasswordChangeController.js";
+import userResetpasswordController from "./userResetpasswordController.js";
+
+export {
+  useremailverifyController,
+  userregisterController,
+  userloginController,
+  userprofileController,
+  userlogoutController,
+  userpasswordChangeController,
+  userResetpasswordController,
+};
---
+++ src/routes/userRoute.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import passport from "passport";
+import getnewToken from "../middlewares/getnewToken.js";
+import {
+  useremailverifyController,
+  userregisterController,
+  userloginController,
+  userprofileController,
+  userlogoutController,
+  userpasswordChangeController,
+  userResetpasswordController,
+} from "../controllers/index.js";
+
+const Authroutes = Router();
+
+// Public Routes
+
+Authroutes.route("/signup").post(userregisterController);
+Authroutes.route("/email-verify").post(useremailverifyController);
+Authroutes.route("/user-login").post(userloginController);
+Authroutes.route("/user-logout").post(userlogoutController);
+Authroutes.route("/user-passwordreset").post(userResetpasswordController);
+
+// Protected Routes
+Authroutes
+  .route("/user-profile")
+  .post(
+    getnewToken,
+    passport.authenticate("jwt", { session: false }),
+    userprofileController
+  );
+Authroutes
+  .route("/user-passwordChange")
+  .post(
+    getnewToken,
+    passport.authenticate("jwt", { session: false }),
+    userpasswordChangeController
+  );
+export { Authroutes };
