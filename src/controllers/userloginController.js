5d91efe72c0faab36d719c1537a055bb38bc073f
Shivam Anand <shivam850anand@gmail.com>
2024-08-11 23:16:01 +0530

    done

--- src/controllers/userloginController.js
+++ src/controllers/userloginController.js
@@ -39,9 +39,6 @@ const userloginController = async (req, res) => {
 
     res.cookie("isUserAuthenticated", user.isAuthenticated, {
       maxAge: 5 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
     });
 
     // Response after user successfully login
