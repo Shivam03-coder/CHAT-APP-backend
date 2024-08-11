59486b639917a7ef1bb8f21739de2e30e6bc7a51
Shivam Anand <shivam850anand@gmail.com>
2024-08-11 23:02:00 +0530

    token

--- src/utils/setTokenscookies.js
+++ src/utils/setTokenscookies.js
@@ -6,13 +6,11 @@ const setTokenscookies = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: true,
     maxAge: ms(appconfig.ACCESS_TOKEN_EXP),
-    sameSite: "None",
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true,
     maxAge: ms(appconfig.REFRESH_TOKEN_EXP),
-    sameSite: "None",
   });
 };
 
